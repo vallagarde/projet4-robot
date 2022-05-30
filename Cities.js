95981056a39759261dad7d1d2275ff1afe163863
Vallagarde <val.lagarde29@gmail.com>
2022-05-30 14:16:43 +0200

    initiaitialisation du robot

---
+++ Cities.js
@@ -0,0 +1,138 @@
+
+class Cities{
+
+    citiesArray = [
+
+        Saint_Jean_De_Luz =  [
+            
+            lat = "43.240",
+            long = "-1.390" 
+        ],
+
+        Biarritz = [
+            
+            lat = "43.310",
+            long ="-1.310"
+
+        ]
+    ]
+
+   /*  Arcachon ={
+       "name" : ,
+        "lat" : 44.40 ,
+        "long" : -1.10
+
+    },
+    Pointe_de_grave ={
+        "name" : ,
+        "lat" : 45.34,
+        "long" : -1.04 
+
+    },
+    La_Rochelle_Pallice ={
+        "name" : ,
+        "lat" : 46.10,
+        "long" :-1.13 
+
+    },
+    Sables_d_Olonne ={
+        "name" : ,
+        "lat" : 46.30,
+        "long" : -1.48 
+
+    },
+    Saint_Nazaire ={
+        "name" : ,
+        "lat" : 47.16,
+        "long" :-2.12 
+
+    },
+    Port_Navalo ={
+        "name" : ,
+        "lat" : 47.33,
+        "long" : -2.55 
+
+    },
+    Ile_de_Groix ={
+        "name" : ,
+        "lat" : 47.39,
+        "long" : -3.27
+
+    },
+    Concarneau ={
+        "name" : ,
+        "lat" : 47.52,
+        "long" : -3.54
+
+    },
+    Brest ={
+        "name" : ,
+        "lat" : 48.23,
+        "long" :- 4.30 
+
+    },
+    Roscoff ={
+        "name" : ,
+        "lat" : 48.43,
+        "long" : -3.58
+
+    },
+    Paimpol ={
+        "name" : ,
+        "lat" :48.48 ,
+        "long" :-2.57 
+
+    },
+    Saint_Malo ={
+        "name" : ,
+        "lat" : 48.38 ,
+        "long" : -2.00 
+
+    },
+    Cherbourg ={
+        "name" : ,
+        "lat" : 49.39 ,
+        "long" : -1.38 
+
+    },
+    Le_Havre ={
+        "name" : ,
+        "lat" : 49.29,
+        "long" : 0.07 
+
+    },
+    Fecamp ={
+        "name" : ,
+        "lat" : 49.46 ,
+        "long" : 0.22 
+
+    },
+    Dieppe ={
+        "name" : ,
+        "lat" : 49.56,
+        "long" : 1.05 
+
+    },
+    Boulogne_sur_Mer ={
+        "name" : ,
+        "lat" : 50.44,
+        "long" : 1.35 
+
+    },
+    Calais ={
+        "name" : ,
+        "lat" : 50.58,
+        "long" : 1.52 
+
+    },
+    Dunkerke ={
+        "name" : ,
+        "lat" : 51.03,
+        "long" : 2.22 
+
+    } */
+
+/* ]; */
+
+}
+module.exports= Cities;
---
+++ server.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient
+var db;
+const https = require('https');
+const WeatherKey = require('./jeys');
+const myjsonCities = require('./cities.json'); //on récupère les données des villes de la cote Atlantique dans le fichier cities.json
+
+let weatherKey = new WeatherKey(); // récupération des clés  pour se connecter aux API
+
+
+
+
+
+
+
+
+/*********************************************************************
+ * Transformation des données du fichier JSON en objets utilisables *
+*********************************************************************/
+
+var jsStringCities =JSON.stringify(myjsonCities);
+console.log(jsStringCities);
+
+var ArrayCities = JSON.parse(jsStringCities); // on a ici un tableau d'objets "ville"  ex: { name: 'Biaritz', lat: 43.31, long: -1.31 }
+
+
+/*********************************************************************
+ ****************** Requetes vers l'API de climat *******************
+*********************************************************************/
+
+for (let city of ArrayCities){
+
+    console.log("on est rentré dans le for "+ city+" "+ city.lat);
+ 
+    https.get('https://api.weatherbit.io/v2.0/current?lat='+city.lat+'&lon='+city.long+'&key='+weatherKey.key +'&lang=fr', (resp) =>{ //requete prenant en compte les prévisions pour les deux jours suivant dans les 20 villes cotières
+    let data='';
+
+    resp.on('data', (chunk) =>{
+        data += chunk;
+    });
+
+    resp.on('end', () => {
+       
+        dataObj = JSON.parse(data);
+        dataObj.city_name = city.name; // dataObj est l'objet contenant les informations météo
+        /*********************************************************************
+        ************ Etablissement de la connection a mongodb **************
+        *********************************************************************/
+          MongoClient.connect('mongodb://localhost:27017', function (err, database) {
+            if (err) 
+              throw err
+            else
+            {
+            var db = database.db('meteodb');
+
+            db.collection('meteo').insertOne(dataObj, function (err, result) {
+              if (err)
+              console.log("error");
+              else
+              console.log("success");
+        
+             });
+            console.log('Connected to MongoDB');
+            }
+          });
+
+       
+
+        console.log(dataObj);
+    });
+    
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+
+})
+
+}
+
+
+/*********************************************************************
+ ****************** Requetes vers l'API de marées *******************
+*********************************************************************/
